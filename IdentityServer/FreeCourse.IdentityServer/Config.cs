1ccb14bd712ce8bd8790b5f651b35c63cc600530
Vedat ERMİŞ <vedatermis@hotmail.com>
2021-08-26 17:07:13 +0300

    commit

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -15,6 +15,8 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resource_photo_stock") {Scopes = {"photo_stock_fullpermission"}},
             new ApiResource("resource_basket") {Scopes = {"basket_fullpermission"}},
             new ApiResource("resource_discount") {Scopes = {"discount_fullpermission"}},
+            new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+            new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
         public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
@@ -37,6 +39,8 @@ namespace FreeCourse.IdentityServer
             new ApiScope("photo_stock_fullpermission", "Photo Stock API için full erişim yetkisi"),
             new ApiScope("basket_fullpermission", "Basket API için full erişim yetkisi"),
             new ApiScope("discount_fullpermission", "Discount API için full erişim yetkisi"),
+            new ApiScope("order_fullpermission", "Order API için full erişim"),
+            new ApiScope("payment_fullpermission", "Payment API için full erişim"),
             new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -65,9 +69,11 @@ namespace FreeCourse.IdentityServer
                 {
                     "basket_fullpermission",
                     "discount_fullpermission",
+                    "order_fullpermission", 
+                    "payment_fullpermission",
                     IdentityServerConstants.LocalApi.ScopeName,
-                    IdentityServerConstants.StandardScopes.OpenId, 
-                    IdentityServerConstants.StandardScopes.Email, 
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Email,
                     IdentityServerConstants.StandardScopes.Profile,
                     IdentityServerConstants.StandardScopes.OfflineAccess,
                     "roles"
